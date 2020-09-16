2e195cf87b7e6968b5683d012e7ae4e0e1ff9e43
thaithong12 <nguyenthaithong98@gmail.com>
2020-09-17 02:11:17 +0700

    [1.0.1] convert to set interval

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 window.onload = function() {
     
     var listTagA = document.getElementsByTagName("a");
+    var listNameImage = [];
+    var listImage = [];
 
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
     demo();
-    async function demo() {
+    function demo() {
         for(let aTag of listTagA) {
                 let img = new Image();
             
                 img.src  = aTag.firstElementChild.src;
                 let nameImage = aTag.firstElementChild.alt;
-        
+
+
+                listNameImage.push(nameImage);
+                listImage.push(img);
+
                 aTag.onclick = function(e) {
                     e.preventDefault()
                 }
-                let textVal = document.getElementById("text-value")
-                let imgShow = document.getElementById("img-show");
-                
-                textVal.innerText = nameImage;
-                imgShow.src = img.src;
-                imgShow.height = 300;
-
-                await sleep(2000);
         }
     }
+
+    var i = 0;
+    var sleepy = setInterval(() => {
+            let textVal = document.getElementById("text-value")
+            let imgShow = document.getElementById("img-show");
+                
+            textVal.innerText = listNameImage[i];
+            imgShow.src = listImage[i].src;
+            imgShow.height = 300;
+            i++
+            if (i == 5) {
+                clearInterval(sleepy);
+            }
+    }, 1000);
    
 }
